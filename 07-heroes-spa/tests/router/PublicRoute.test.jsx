92598a876f53b5cb9fd8ec52b03168482670b79a
Daniel Guasamucaro <daniel.guasamucaro@binariaseguros.com.ar>
2022-07-25 17:37:28 -0300

    222 Pruebas en el PublicRoute - Parte 2

---
+++ 07-heroes-spa/tests/router/PublicRoute.test.jsx
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react';
+import { AuthContext } from '../../src/auth';
+import { PublicRoute } from '../../src/router/PublicRoute';
+
+describe('Pruebas en <PublicRoute />', () => {
+
+    test('debe de mostrar el children si no está autenticado', () => {
+
+        const contextValue = {
+            logged: false
+        }
+
+        render(
+            <AuthContext.Provider value={contextValue}>
+                <PublicRoute>
+                    <h1>Ruta pública</h1>
+                </PublicRoute>
+            </AuthContext.Provider>
+        );
+
+        expect(screen.getByText('Ruta pública')).toBeTruthy();
+
+    });
+
+    test('debe de navegar si está autenticado', () => {
+
+        
+
+    });
+
+});
